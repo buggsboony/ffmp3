cfca0d0f12f49b3003cd998ba6a3a287762e86c3
buggsboony <bruno.lesdema@gmail.com>
2021-05-31 17:35:13 -0400

    ogg conversion added with ffogg

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 #install stuff
 what=ffmp3
+what2=ffogg
 extension=.sh
 #peut être extension vide
 
@@ -9,4 +10,5 @@ echo "Rendre exécutable..."
 chmod +x $what$extension
 echo "lien symbolique vers usr bin"
 sudo ln -s "$PWD/$what$extension" /usr/bin/$what
+sudo ln -s "$PWD/$what2$extension" /usr/bin/$what2
 
